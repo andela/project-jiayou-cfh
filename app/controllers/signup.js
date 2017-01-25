ee9e1f4c7dbb3efe24009f0fbde9e7212cad7cc0
Fisayomi Ojuri <Fisayomi@Fisayomis-MacBook-Pro.local>
2017-01-25 13:58:06 +0100

    JWT sign-up Refactored

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,14 +1,13 @@
 var mongoose = require('mongoose'),
-<<<<<<< HEAD
 User = mongoose.model('User');
 var avatars = require('./avatars').all();
 var moment = require('moment');
 var jwt = require('jsonwebtoken');
 var secret = process.env.JWT_SECRET;
 
-exports.signup = function(req, res) {
+exports.signup = function (req, res) {
  // check if req has all the data
-	if (req.body.username && req.body.password && req.body.email) {
+ if (req.body.username && req.body.password && req.body.email) {
   User.findOne({email:req.body.email}, function(err, existingUser){
    if (err) {
     throw err;
@@ -16,7 +15,7 @@ exports.signup = function(req, res) {
    }
    if (existingUser) {
     res.send({success: false, mess: 'Already a user'});
-   }else{
+   } else {
     var user = new User(req.body);
     user.avatar = avatars[user.avatar];
     user.provider = 'local';
@@ -37,55 +36,7 @@ exports.signup = function(req, res) {
     exp: exp});
    }
   });
- }else{
-  return res.redirect('/#!/signup?error=incomplete');
- }
-=======
-  User = mongoose.model('User');
-  var avatars = require('./avatars').all();
-
-var moment = require('moment');
-var jwt = require('jsonwebtoken');
-
-var secret = process.env.JWT_SECRET;
-
-exports.signup = function(req, res) {
-	// check if reg has all the data
-	if (req.body.name && req.body.password && req.body.email) {
-		User.findOne({email:req.body.email}, function(err, exUser){
-			if(err){
-				throw err;
-			}
-			if(exUser){
-				res.send({success: false, mess: 'Already a user'});
-			}
-			else{
-				var user = new User(req.body);
-				user.avatar = avatars[user.avatar];
-	        	user.provider = 'local';
-	        	user.save(function(err) {
-			          if (err) {
-			            return res.render('/#!/signup?error=unknown', {
-			              errors: err.errors,
-			              user: user
-			            });
-			          }
-			         });
-				var exp = moment().add(5, 'hours').valueOf();
-				var token = jwt.sign({userId: user._id, exp:exp}, secret);
-				res.json({success: true, mss: 'New user created', 
-					token: token,
-					email:user.email,
-					username:user.name,
-					exp: exp});
-				
-				//return res.redirect('/#!/');
-			}
-	    });
-	  }
-	  else{
-	  	return res.redirect('/#!/signup?error=incomplete');
-	  }
-    
->>>>>>> c8f3b6561eb76f9080c8104f6bc1d85fa94d8f18
+ } else {
+   return res.redirect('/#!/signup?error=incomplete');
+  }
 };
