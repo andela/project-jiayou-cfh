7c5115c642f9026aa0037c6b81ea1a132a2be3c4
andela-rbabalola <rotimi.babalola@andela.com>
2017-01-25 17:00:48 +0100

    Styled my code to meet airbnb specs

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -1,30 +1,27 @@
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+  User = mongoose.model('User');
 var moment = require('moment');
 var jwt = require('jsonwebtoken');
 var secret = process.env.SECRET;
 var dotEnv = require('dotenv');
 dotEnv.config();
 
-exports.userAuth = function(req, res){
-	User.findOne({
-		email: req.body.email
-	}, function(err, user){
-		if(err){
-			return res.json(err);
-		} 
-		if(!user){
-			res.send({success: false, msg: 'Authentication failed user not found'});
-		} else {
-			if(user.authenticate(req.body.password)){
-				var token = jwt.sign(user, secret);
-				//use moment to state that the token will last for 5 hours
-				res.json({success: true, token: 'JWT ' + token, expDate: moment().add(5, 'hours').valueOf()});
-			} else {
-				res.send({success: false, message: 'Authentication failed wrong password'});
-			}
-		}
-
-	});
+exports.userAuth = function (req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function (err, user) {
+    if (err) {
+      return res.json({ success: false, msg: 'An unexpected error occurred' });
+    }
+    if (!user) {
+      res.send({ success: false, msg: 'Authentication failed user not found' });
+    } else if (user.authenticate(req.body.password)) {
+      var token = jwt.sign(user, secret);
+        // use moment to state that the token will last for 5 hours
+      res.json({ success: true, token: `${token}`, userEmail: req.body.email, expDate: moment().add(5, 'hours').valueOf() });
+    } else {
+      res.send({ success: false, message: 'Authentication failed wrong password' });
+    }
+  });
 };
 
