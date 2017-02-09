13d919b63d89d43b6c85510ec86044f2b032e376
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-02-09 16:14:00 +0100

    History page

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,64 @@
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives', 'checklist-model'])
   .config(['$routeProvider',
-    function ($routeProvider) {
+    function($routeProvider) {
       $routeProvider.
-        when('/', {
-          templateUrl: 'views/index.html'
-        }).
-        when('/app', {
-          templateUrl: '/views/app.html',
-        }).
-        when('/privacy', {
-          templateUrl: '/views/privacy.html',
-        }).
-        when('/bottom', {
-          templateUrl: '/views/bottom.html'
-        }).
-        when('/signin', {
-          templateUrl: '/views/signin.html'
-        }).
-        when('/signup', {
-          templateUrl: '/views/signup.html'
-        }).
-        when('/choose-avatar', {
-          templateUrl: '/views/choose-avatar.html'
-        }).
-        when('/charity', {
-          templateUrl: '/views/charity.html'
-        }).
-        when('/aboutus', {
-          templateUrl: '/views/aboutus.html'
-        }).
-        when('/signin-up', {
-          templateUrl: '/views/signin-up.html'
-        }).
-        when('/gametour', {
-          templateUrl: '/views/tour.html'
-        }).
-        otherwise({
-          redirectTo: '/'
-        });
+      when('/', {
+        templateUrl: 'views/index.html'
+      }).
+      when('/app', {
+        templateUrl: '/views/app.html',
+      }).
+      when('/privacy', {
+        templateUrl: '/views/privacy.html',
+      }).
+      when('/bottom', {
+        templateUrl: '/views/bottom.html'
+      }).
+      when('/signin', {
+        templateUrl: '/views/signin.html'
+      }).
+      when('/signup', {
+        templateUrl: '/views/signup.html'
+      }).
+      when('/choose-avatar', {
+        templateUrl: '/views/choose-avatar.html'
+      }).
+      when('/charity', {
+        templateUrl: '/views/charity.html'
+      }).
+      when('/aboutus', {
+        templateUrl: '/views/aboutus.html'
+      }).
+      when('/signin-up', {
+        templateUrl: '/views/signin-up.html'
+      }).
+      when('/gametour', {
+        templateUrl: '/views/tour.html'
+      }).
+      when('/history', {
+        templateUrl: '/views/history.html'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
     }
   ]).config(['$locationProvider',
-    function ($locationProvider) {
+    function($locationProvider) {
       $locationProvider.hashPrefix("!");
     }
-  ]).run(['$rootScope', function ($rootScope) {
-    $rootScope.safeApply = function (fn) {
+  ]).run(['$rootScope', function($rootScope) {
+    $rootScope.safeApply = function(fn) {
       var phase = this.$root.$$phase;
       if (phase === '$apply' || phase === '$digest') {
-        if (fn && (typeof (fn) === 'function')) {
+        if (fn && (typeof(fn) === 'function')) {
           fn();
         }
       } else {
         this.$apply(fn);
       }
     };
-  }]).run(['DonationService', function (DonationService) {
-    window.userDonationCb = function (donationObject) {
+  }]).run(['DonationService', function(DonationService) {
+    window.userDonationCb = function(donationObject) {
       DonationService.userDonated(donationObject);
     };
   }]);
