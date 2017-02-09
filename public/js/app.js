78e6bf49551ecd705139df7cd832730f4cd76cc6
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-02-09 13:03:03 +0100

    Dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'mean.system', 'mean.directives'])
   .config(['$routeProvider',
-    function ($routeProvider) {
+    function($routeProvider) {
       $routeProvider.
       when('/', {
         templateUrl: 'views/index.html'
@@ -32,27 +32,30 @@ angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'm
       when('/signin-up', {
         templateUrl: '/views/signin-up.html'
       }).
+      when('/history', {
+        templateUrl: '/views/history.html'
+      }).
       otherwise({
         redirectTo: '/'
       });
     }
   ]).config(['$locationProvider',
-    function ($locationProvider) {
+    function($locationProvider) {
       $locationProvider.hashPrefix('!');
     }
-  ]).run(['$rootScope', function ($rootScope) {
-    $rootScope.safeApply = function (fn) {
+  ]).run(['$rootScope', function($rootScope) {
+    $rootScope.safeApply = function(fn) {
       var phase = this.$root.$$phase;
       if (phase === '$apply' || phase === '$digest') {
-        if (fn && (typeof (fn) === 'function')) {
+        if (fn && (typeof(fn) === 'function')) {
           fn();
         }
       } else {
         this.$apply(fn);
       }
     };
-  }]).run(['DonationService', function (DonationService) {
-    window.userDonationCb = function (donationObject) {
+  }]).run(['DonationService', function(DonationService) {
+    window.userDonationCb = function(donationObject) {
       DonationService.userDonated(donationObject);
     };
   }]);
