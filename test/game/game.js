9d11bc1bea29ce8859d061f7e4431431ae889735
elahsoft <febechukwu.opara@andela.com>
2017-02-17 11:53:27 +0100

    port changed #137138317

--- test/game/game.js
+++ test/game/game.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var io = require('socket.io-client');
 
-var socketURL = 'http://localhost:5000';
+var socketURL = 'http://localhost:3000';
 
 var options ={
   transports: ['websocket'],
